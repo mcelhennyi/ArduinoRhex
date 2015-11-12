603e2f813c60349d5ddc9cbf37b674d0e4b11d31
Ian McElhenny <mcelhenny_ian@yahoo.com>
2015-11-11 23:57:05 -0500

    added gps

---
+++ TestCode/GPS/GPS.ino
@@ -0,0 +1,69 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_GPS.h>
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
+
+String NMEA1;
+String NMEA2;
+char c;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  GPS.begin(9600);
+  
+  GPS.sendCommand("$PGCMD, 33, 0*6D");//Turn off antenna update data
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //Set uprate to 1 hz
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //request RMC and GGA sentences only
+  delay(1000);
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  readGPS();
+  
+  delay(2000);
+  
+}
+
+void readGPS()
+{
+  clearOldData();
+  clearOldData();
+
+  while(!GPS.newNMEAreceived())
+  {
+    c=GPS.read();
+  } 
+  GPS.parse(GPS.lastNMEA());
+  NMEA1 = GPS.lastNMEA();
+
+  while(!GPS.newNMEAreceived())
+  {
+    c=GPS.read();
+  } 
+  GPS.parse(GPS.lastNMEA());
+  NMEA2 = GPS.lastNMEA();  
+  
+  Serial.println(NMEA1);
+  Serial.println(NMEA2);
+  
+}
+
+void clearOldData()
+{
+  while(!GPS.newNMEAreceived())
+  {
+    c=GPS.read();
+  } 
+  GPS.parse(GPS.lastNMEA());
+
+  while(!GPS.newNMEAreceived())
+  {
+    c=GPS.read();
+  } 
+  GPS.parse(GPS.lastNMEA());
+}
